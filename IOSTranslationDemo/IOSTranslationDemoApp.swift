c9b9fe887b8de790a76ae7ce2fd1da27fff5deea
Osama Mersal <osamamersal@Osamas-Mac-mini.local>
2025-07-13 11:29:10 +0300

    Initial commit: Simple demo app with hardcoded English strings, no localization.

---
+++ IOSTranslationDemo/IOSTranslationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSTranslationDemoApp.swift
+//  IOSTranslationDemo
+//
+//  Created by Osama Mersal on 13/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct IOSTranslationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IOSTranslationDemoTests/IOSTranslationDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  IOSTranslationDemoTests.swift
+//  IOSTranslationDemoTests
+//
+//  Created by Osama Mersal on 13/07/2025.
+//
+
+import Testing
+@testable import IOSTranslationDemo
+
+struct IOSTranslationDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
